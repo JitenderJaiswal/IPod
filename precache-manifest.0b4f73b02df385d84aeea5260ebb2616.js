4fd7cb457eccd0865513bbd137e5b438939240e0
jitender jaiswal <jitenderjaiswal686@gmail.com>
2020-09-18 07:34:40 -0400

    Updates

---
+++ precache-manifest.0b4f73b02df385d84aeea5260ebb2616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d01799abb21cce76bb332c3ce25c4e",
+    "url": "/IPod/index.html"
+  },
+  {
+    "revision": "defa031cd23eeb7842ac",
+    "url": "/IPod/static/css/main.8ac66c4d.chunk.css"
+  },
+  {
+    "revision": "8b7f9aca9465137d5ad4",
+    "url": "/IPod/static/js/2.4ef3e3ee.chunk.js"
+  },
+  {
+    "revision": "0395921f8b56d12a4870f8ea70bf3c15",
+    "url": "/IPod/static/js/2.4ef3e3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "defa031cd23eeb7842ac",
+    "url": "/IPod/static/js/main.af3a0926.chunk.js"
+  },
+  {
+    "revision": "3b8ce0ce9deefad1581e",
+    "url": "/IPod/static/js/runtime-main.f2960962.js"
+  }
+]);
